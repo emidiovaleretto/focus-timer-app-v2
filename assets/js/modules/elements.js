436028c00ded8054370d1c9e6bda3a288275365a
Emidio Valereto <emidio.valereto@gmail.com>
2023-07-04 18:52:39 +0100

    chore: create an elements module

---
+++ assets/js/modules/elements.js
@@ -0,0 +1,32 @@
+export function Elements() {
+  const play = document.querySelector(".button-play")
+  const stop = document.querySelector(".button-stop")
+  const increase = document.querySelector(".button-increase")
+  const decrease = document.querySelector(".button-decrease")
+
+  const piano = document.querySelector(".button-piano")
+  const rain = document.querySelector(".button-rain")
+  const waves = document.querySelector(".button-ocean")
+  const fire = document.querySelector(".button-fire")
+
+  let minutesOnDisplay = document.querySelector(".minutes")
+  let secondsOnDisplay = document.querySelector(".seconds")
+
+  let countdown, minutes, seconds
+
+  return {
+    play,
+    stop,
+    increase,
+    decrease,
+    piano,
+    rain,
+    waves,
+    fire,
+    minutesOnDisplay,
+    secondsOnDisplay,
+    countdown,
+    minutes,
+    seconds,
+  }
+}
