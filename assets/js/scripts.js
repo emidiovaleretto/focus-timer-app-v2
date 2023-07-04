125f7c3bb8e80171f3c389a9b4448a89356cee4a
Emidio Valereto <emidio.valereto@gmail.com>
2023-07-04 06:35:26 +0100

    chore: add stop button functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,25 @@
 const buttonPlay = document.querySelector(".button-play")
 const buttonStop = document.querySelector(".button-stop")
-const buttonIncrease = document.querySelector("button-increase")
-const buttonDecrease = document.querySelector("button-decrease")
+const buttonIncrease = document.querySelector(".button-increase")
+const buttonDecrease = document.querySelector(".button-decrease")
 
 const minutesOnDisplay = document.querySelector(".minutes")
 const secondsOnDisplay = document.querySelector(".seconds")
 
 const buttonPiano = document.querySelector(".button-piano")
 
-let minutes, seconds, countdown
+let countdown
+
+let minutes = minutesOnDisplay.innerHTML
+let seconds = secondsOnDisplay.innerHTML
+
+function updateTimerDisplay() {
+  minutesOnDisplay.innerHTML = String(minutes).padStart(2, "0")
+  secondsOnDisplay.innerHTML = String(seconds).padStart(2, "0")
+}
 
 buttonPlay.onclick = () => {
   countdown = setInterval(() => {
-    minutes = minutesOnDisplay.innerHTML.padStart(2, "0")
-    seconds = secondsOnDisplay.innerHTML.padStart(2, "0")
-
     if (minutes === 0 && seconds === 0) {
       clearInterval(countdown)
       return
@@ -27,11 +32,15 @@ buttonPlay.onclick = () => {
       seconds--
     }
 
-    minutesOnDisplay.innerHTML = String(minutes).padStart(2, "0")
-    secondsOnDisplay.innerHTML = String(seconds).padStart(2, "0")
+    updateTimerDisplay()
   }, 1000)
+
+  buttonPlay.classList.remove("button-play")
+  buttonPlay.classList.add("button-pause")
 }
 
 buttonStop.onclick = () => {
+  buttonPlay.classList.remove("button-pause")
+  buttonPlay.classList.add("button-play")
   clearInterval(countdown)
 }
